3de4d9d2e08fa632450faed7f87c65dceed8381d
Tim Tennant <ttennant@gmail.com>
2011-03-04 14:57:30 +0000

    Adding most common html tags

--- src/main/java/xpather/XpathHtmlElement.java
+++ src/main/java/xpather/XpathHtmlElement.java
@@ -11,12 +11,28 @@ public class XpathHtmlElement {
         return element("head");
     }
 
+    public static XpathElement body() {
+        return element("body");
+    }
+
     public static XpathElement title() {
         return element("title");
     }
 
     public static XpathElement div() {
         return element("div");
+    }
+
+    public static XpathElement span() {
+        return element("span");
+    }
+
+    public static XpathElement p() {
+        return element("p");
+    }
+
+    public static XpathElement h(int level) {
+        return element("h"+level);
     }
 
     static XpathElement ul(){
@@ -29,6 +45,45 @@ public class XpathHtmlElement {
 
     static XpathElement anchor(){
         return element("a");
+    }
+
+    static XpathElement table(){
+        return element("table");
+    }
+
+    static XpathElement th(){
+        return element("th");
+    }
+
+    static XpathElement tr(){
+        return element("tr");
+    }
+
+    static XpathElement td(){
+        return element("td");
+    }
+
+    static XpathElement img(){
+        return element("img");
+    }
+
+    static XpathElement form(){
+        return element("form");
+    }
+
+    static XpathElement input(){
+        return element("input");
+    }
+
+    static XpathElement textarea(){
+        return element("textarea");
+    }
+
+    static XpathElement select(){
+        return element("select");
+    }
+    static XpathElement option(){
+        return element("option");
     }
 
 }
